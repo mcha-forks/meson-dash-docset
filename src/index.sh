3c6bd67b02dfb5dd7ac11455741530c3ebd034fa
Louie S <louie@example.com>
2023-05-05 19:25:50 -0700

    Create lib subdirectory for common functions

--- src/index.sh
+++ src/index.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
+# shellcheck source=./lib/create_table
+. "$(dirname "$0")"/lib/create_table
+# shellcheck source=./lib/insert
+. "$(dirname "$0")"/lib/insert
+
 DB_PATH="$1"
 shift
-
-create_table() {
-	sqlite3 "$DB_PATH" "CREATE TABLE IF NOT EXISTS searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);"
-	sqlite3 "$DB_PATH" "CREATE UNIQUE INDEX IF NOT EXISTS anchor ON searchIndex (name, type, path);"
-}
-
 
 get_title() {
 	FILE="$1"
@@ -15,14 +14,6 @@ get_title() {
 	pup -p -f "$FILE" 'title text{}' | \
 		tr -d \\n | \
 		sed 's/\"/\"\"/g'
-}
-
-insert() {
-	NAME="$1"
-	TYPE="$2"
-	PAGE_PATH="$3"
-
-	sqlite3 "$DB_PATH" "INSERT INTO searchIndex(name, type, path) VALUES (\"$NAME\",\"$TYPE\",\"$PAGE_PATH\");"
 }
 
 insert_pages() {
@@ -33,11 +24,11 @@ insert_pages() {
 		PAGE_NAME="$(get_title "$1")"
 		PAGE_TYPE="Guide"
 		if [ -n "$PAGE_NAME" ]; then
-			insert "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
+			insert "$DB_PATH" "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
 		fi
 		shift
 	done
 }
 
-create_table
+create_table "$DB_PATH"
 insert_pages "$@"
